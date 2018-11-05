489b1d2b9351965b3eab60871ddf898e79bb6794
jack <steve.a.shogren@gmail.com>
2018-11-05 06:31:36 -0500

    More work on testing nested enums

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,61 @@
 use std::io;
 extern crate sqlpop;
 use sqlpop::parser::parse_sql;
+use sqlpop::ast::Cmd;
 
 fn main() {
     println!("Starting sql repl");
+    parse("select * from users;");
 
-    while true {
+    loop {
         let mut input = String::new();
         match io::stdin().read_line(&mut input) {
-            Ok(n) => {
-                println!("{}", input);
-                let x = parse_sql("Select * from users;").unwrap();
-                println!("{:?}", x);
+            Ok(_) => {
+                parse(&input);
             }
             Err(error) => println!("error: {}", error),
         }
     }
 }
+
+fn parse(s:&str) -> Vec<Option<Cmd>> {
+    let x =  parse_sql(s);
+    match x {
+        Ok(stmt) => {
+            println!("{:?}", stmt);
+            return stmt;
+        },
+        Err(er) => {
+            println!("-------- Malformed sql! ---------");
+            println!("{:?}", er);
+            panic!("Malformed sql!");
+        },
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use sqlpop::ast::*;
+
+
+    #[test]
+    fn test_select() {
+        let x = parse("Select * from users;");
+        println!("{:?}", x);
+        let y = x.first();
+        if let Some(stmt) = y {
+            match stmt {
+                Select {with: w,
+                        body: bod,
+                        order_by: ord,
+                        limit: lim} => {assert_eq!(1,2),},
+                _ => assert_eq!(1,2),
+            };
+        };
+        assert_eq!(1,2);
+    }
+}
+
+
